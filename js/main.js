413a85fb8dab03e74e62057ff8afc36f43a0c02a
pedrogmo <42345539+pedrogmo@users.noreply.github.com>
2019-02-25 10:50:04 -0300

    deu ruimn ranmois

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
-
-document.getElementById('h').mouseover = function() {
-    
+var mostrar = true;
+document.getElementById("mostrarMenu").onclick = function(){
+    if (mostrar)
+        document.getElementById("menuV").style.left = 0;
+    else
+    {
+        document.getElementById("menuV").style.left = '-30em';
+    }
+    mostrar = !mostrar;
 }
