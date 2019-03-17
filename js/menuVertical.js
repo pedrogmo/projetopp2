d4a76a7d8abafe1229113eec7734a68beb6d2eb8
VrRms <vitorramos2003@gmail.com>
2019-03-16 22:20:12 -0300

    vitas

--- js/menuVertical.js
+++ js/menuVertical.js
@@ -4,6 +4,7 @@
 //////////////////////////////////////////////////////////////////
 
 var mostrar = true;
+var LoginAberto = false;
 document.getElementById("telaEscura").style.visibility = "hidden";
 
 $(document).ready(function(){
@@ -11,6 +12,15 @@ $(document).ready(function(){
         $('.icon').toggleClass('active');
     });
 });
+
+document.getElementById("btnLogin").onclick = function()
+{
+    document.getElementById("menuV").style.left = "-30em";
+    mostrar = false;
+
+    document.getElementById("modalLogin").style.visibility = 'initial';
+    LoginAberto = true;
+}
 
 document.getElementById("mostrarMenu").onclick = function(){
     if (mostrar)
@@ -23,7 +33,13 @@ document.getElementById("mostrarMenu").onclick = function(){
     {
         //escondido
         document.getElementById("menuV").style.left = '-30em';
-        document.getElementById("telaEscura").style.visibility = "hidden";
+        document.getElementById("telaEscura").style.visibility = "hidden";   
+        
+        if(LoginAberto)
+        {
+            document.getElementById("modalLogin").style.visibility = 'hidden';
+            LoginAberto = false;
+        }
     }
     mostrar = !mostrar;
 }
@@ -40,12 +56,16 @@ document.getElementById("mostrarMenu").onclick = function(){
 
 document.getElementById("telaEscura").onclick = function()
 {
-    document.getElementById("menuV").style.left = '-30em';
-    document.getElementById("telaEscura").style.visibility = "hidden";
-    mostrar = !mostrar;
-
-    var menu = document.querySelector('.icon')
-    menu.classList.toggle('active');
+    if(!LoginAberto)
+    {
+        document.getElementById("menuV").style.left = '-30em';
+        document.getElementById("telaEscura").style.visibility = "hidden";
+        mostrar = !mostrar;
+    
+        var menu = document.querySelector('.icon')
+        menu.classList.toggle('active');
+    }
+    
 }
 
 //////////////////////////////////////////////////////////////////
