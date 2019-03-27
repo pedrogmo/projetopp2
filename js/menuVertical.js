6e80072edb9df5eb8de543eccfe68df230387b90
VrRms <vitorramos2003@gmail.com>
2019-03-27 11:29:56 -0300

    R1c4rd0M1l0s

--- js/menuVertical.js
+++ js/menuVertical.js
@@ -4,8 +4,10 @@
 //////////////////////////////////////////////////////////////////
 
 var mostrar = true;
-var LoginAberto = false;
+var loginAberto = false
+
 document.getElementById("telaEscura").style.visibility = "hidden";
+document.getElementById("telaEscura-desk").style.visibility = "hidden";
 
 $(document).ready(function(){
     $('.icon').click(function(){
@@ -15,20 +17,29 @@ $(document).ready(function(){
 
 document.getElementById("btnLogin").onclick = function()
 {
-    document.getElementById("menuV").style.left = "-30em";
-    mostrar = false;
+    loginAberto = !loginAberto;
 
-    document.getElementById("modalLogin").style.visibility = 'initial';
-    LoginAberto = true;
+    if(loginAberto)
+    {
+        document.getElementById("modalLogin").style.visibility = 'initial';
+        document.getElementById("menuV").style.left = '-30em';
+        document.getElementById("telaEscura-desk").style.visibility = "initial";
+    }
+    else
+        document.getElementById("modalLogin").style.visibility = 'hidden';
+        
 }
 
 document.getElementById("btnLogin2").onclick = function()
 {
-    document.getElementById("menuV").style.left = "-30em";
-    mostrar = false;
-
-    document.getElementById("modalLogin").style.visibility = 'initial';
-    LoginAberto = true;
+    loginAberto = !loginAberto;
+    
+    if(loginAberto)
+    {
+        document.getElementById("modalLogin").style.visibility = 'initial';
+        document.getElementById("telaEscura-desk").style.visibility = "initial";
+        document.getElementById("telaEscura").style.visibility = "initial";
+    }         
 }
 
 document.getElementById("mostrarMenu").onclick = function(){
@@ -44,10 +55,10 @@ document.getElementById("mostrarMenu").onclick = function(){
         document.getElementById("menuV").style.left = '-30em';
         document.getElementById("telaEscura").style.visibility = "hidden";   
         
-        if(LoginAberto)
+        if(loginAberto)
         {
             document.getElementById("modalLogin").style.visibility = 'hidden';
-            LoginAberto = false;
+            loginAberto = false;
         }
     }
     mostrar = !mostrar;
@@ -65,7 +76,7 @@ document.getElementById("mostrarMenu").onclick = function(){
 
 document.getElementById("telaEscura").onclick = function()
 {
-    if(!LoginAberto)
+    if(!loginAberto)
     {
         document.getElementById("menuV").style.left = '-30em';
         document.getElementById("telaEscura").style.visibility = "hidden";
@@ -74,7 +85,19 @@ document.getElementById("telaEscura").onclick = function()
         var menu = document.querySelector('.icon')
         menu.classList.toggle('active');
     }
+}
+
+document.getElementById("telaEscura-desk").onclick = function()
+{
+    if(loginAberto)
+    {
+        document.getElementById("telaEscura-desk").style.visibility = "hidden";
+        document.getElementById("modalLogin").style.visibility = 'hidden';
+        loginAberto = false;
     
+        var menu = document.querySelector('.icon')
+        menu.classList.toggle('active');
+    }
 }
 
 //////////////////////////////////////////////////////////////////
