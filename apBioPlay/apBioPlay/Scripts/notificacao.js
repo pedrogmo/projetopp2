2e27c556a400a56111c5b7da890f15eaf73834c6
VrRms <vitorramos2003@gmail.com>
2019-05-21 10:26:40 -0300

    vrau

---
+++ apBioPlay/apBioPlay/Scripts/notificacao.js
@@ -0,0 +1,15 @@
+﻿
+var modal = document.getElementById("bg-modal");
+var sino = document.getElementById("notificacao");
+var EstaAberto = false;
+
+document.getElementById("close").onclick = function () {
+    modal.style.visibility = "hidden";
+    var EstaAberto = false;
+}
+
+sino.onclick = function () {
+    modal.style.visibility = "initital";
+    var EstaAberto = true;
+}
+
