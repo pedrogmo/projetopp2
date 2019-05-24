e58fc21aff6035fa71b00849cae1f24d01aef7e8
pedrogmo <42345539+pedrogmo@users.noreply.github.com>
2019-05-24 10:59:00 -0300

    javascript genérico :)

--- apBioPlay/apBioPlay/Scripts/pergunta.js
+++ apBioPlay/apBioPlay/Scripts/pergunta.js
@@ -1,30 +1,19 @@
 ﻿
 var selecionada = document.getElementById("selecionada");
 
-/* OPÇÕES */
+/*var pics = document.getElementsByName("pic");
 
-var pic1 = document.getElementById("pic1");
-var pic2 = document.getElementById("pic2");
-var pic3 = document.getElementById("pic3");
-var pic4 = document.getElementById("pic4");
-var pic5 = document.getElementById("pic5");
+for (var i = 0; i < pics.length; ++i)
+    pics[i].onclick = clique(i);
 
-pic1.onclick = function () {
-    selecionada.value = "0";
-}
+function clique(a) {
+    selecionada.value = a + "";
+}*/
 
-pic2.onclick = function () {
-    selecionada.value = "1";
-}
-
-pic3.onclick = function () {
-    selecionada.value = "2";
-}
-
-pic4.onclick = function () {
-    selecionada.value = "3";
-}
-
-pic4.onclick = function () {
-    selecionada.value = "4";
-}
+$(document).ready(function () {
+    $("input[name='pic']").each(function (index) {
+        $(this).on("click", function () {
+            selecionada.value = index + "";
+        });
+    });
+})
