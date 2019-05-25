18c9e54694b5bd0a97f8bcfa95f6916941ea8563
VrRms <vitorramos2003@gmail.com>
2019-05-25 11:16:40 -0300

    adwaadwdaw

--- apBioPlay/apBioPlay/Scripts/modalAmigos.js
+++ apBioPlay/apBioPlay/Scripts/modalAmigos.js
@@ -2,14 +2,30 @@
 var sair = document.getElementById("sair");
 var modal = document.getElementById("black-content");
 var btnNovo = document.getElementById("new");
+var btnBuscar = document.getElementById("buscar");
+var temResultado = document.getElementById("temResultado").value;
+var caixa = document.getElementById("caixa");
 var estaAberto = false;
 
+window.onload = function () {
+        modal.classList.add("invisivel");
+        /*sessionStorage.removeItem('temResultado');*/
+       
+}
+
 sair.onclick = function () {
-    modal.style.visibility = "hidden";
-    estaAberto = false;
+    modal.classList.add("invisivel");
+
+    if (sessionStorage.getItem("temResultado") != null)
+        sessionStorage.removeItem('temResultado');
 }
 
 btnNovo.onclick = function () {
-    modal.style.visibility = "initial";
+    modal.classList.remove("invisivel");
+}
+
+btnBuscar.onclick = function () {
+    modal.classList.remove("invisivel");
+    sessionStorage.setItem("temResultado", temResultado);
     estaAberto = true;
 }
