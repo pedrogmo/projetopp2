9ec241605a4e5a32a0050b70bbfc0ac6826ad0ba
VrRms <vitorramos2003@gmail.com>
2019-05-24 22:01:17 -0300

    Pedro, faz a estrutura da lista dos usuários procurados pra mim pufavô, não to conseguindo puxar da viewbag

---
+++ apBioPlay/apBioPlay/Scripts/modalAmigos.js
@@ -0,0 +1,15 @@
+﻿
+var sair = document.getElementById("sair");
+var modal = document.getElementById("black-content");
+var btnNovo = document.getElementById("new");
+var estaAberto = false;
+
+sair.onclick = function () {
+    modal.style.visibility = "hidden";
+    estaAberto = false;
+}
+
+btnNovo.onclick = function () {
+    modal.style.visibility = "initial";
+    estaAberto = true;
+}
