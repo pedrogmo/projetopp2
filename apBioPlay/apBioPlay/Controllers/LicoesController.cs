fc67e03e8f302ede0850c2b5c8eca8fb5777c32b
pedrogmo <42345539+pedrogmo@users.noreply.github.com>
2019-05-29 10:37:58 -0300

    js e Controler

--- apBioPlay/apBioPlay/Controllers/LicoesController.cs
+++ apBioPlay/apBioPlay/Controllers/LicoesController.cs
@@ -248,6 +248,12 @@ namespace apBioPlay.Controllers
             return RedirectToAction("Index", "Home");
         }
 
+        public ActionResult ClicouExcluir()
+        {
+            Session["excluir"] = true;
+            return RedirectToAction("Perfil");
+        }
+
         public ActionResult ExcluirConta()
         {
             new UsuariosDAO().Remover(((Usuario)Session["usuarioLogado"]).Codigo);
---
+++ apBioPlay/apBioPlay/Scripts/exclusaoConta.js
@@ -0,0 +1,8 @@
+﻿var confirm = document.getElementById("modal-confirm")
+var exibirConfirm = document.getElementById("exibirExcluir").innerHTML;
+
+if (exibirConfirm == 'N') {
+    confirm.style.visibility = "hidden";
+} else if (exibirConfirm == 'S') {
+    confirm.style.visibility = "initial";
+}
