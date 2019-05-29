7f53560053c39c59737b3449e7c9b248a9fcfbf5
pedrogmo <42345539+pedrogmo@users.noreply.github.com>
2019-05-29 11:25:56 -0300

    una cosita

--- apBioPlay/apBioPlay/Models/Paragrafo.cs
+++ apBioPlay/apBioPlay/Models/Paragrafo.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace apBioPlay.Models
 {
     public class Paragrafo
     {
+        [Key]
         public int Codigo { get; set; }
         public string Texto { get; set; }
         public int CodLicao { get; set; }
