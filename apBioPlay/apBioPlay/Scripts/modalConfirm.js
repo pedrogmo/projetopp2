50135b8e260f9c4eedb6c3d5b14d5187ffdc4d90
VrRms <vitorramos2003@gmail.com>
2019-06-06 09:15:54 -0300

    ta aí fi

---
+++ apBioPlay/apBioPlay/Scripts/modalConfirm.js
@@ -0,0 +1,12 @@
+﻿
+var nao = document.getElementById("nao");
+var modal = document.getElementById("bg-modal");
+var voltar = document.getElementById("voltar");
+
+nao.onclick = function () {
+    modal.style.visibility = "hidden";
+}
+
+voltar.onclick = function () {
+    modal.style.visibility = "initial";
+}
